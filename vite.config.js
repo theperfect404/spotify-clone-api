a79d43b738c45d9c9687957e2726cc8339676836
Kaiz <realkaiz7@gmail.com>
2024-01-20 19:23:49 +0800

    update: `vite.config.js`

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/spotify-clone-api/" : "/",
+  base: "/spotify-clone-api/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
